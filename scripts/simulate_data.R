8d06c5552628841154ec04fdffeb8c7a5a0072c8
ricvolpe <volpatorv@hotmail.com>
2026-05-31 20:19:40 +0100

    Drafted new EFA over pilot data, almost ready for pre-reg

--- scripts/simulate_data.R
+++ scripts/simulate_data.R
@@ -1,26 +1,18 @@
 library(tidyverse)
 library(faux)
-source("read_qualtrics_data.R")
 
 options(readr.show_col_types = FALSE)
 
-simulate_data <- function(sample_path, factors_path, seed = 8675309, shrinkage = 0.85, n = 200) {
+simulate_data <- function(data, seed = 8675309, shrinkage = 0.85, n = 200) {
   set.seed(seed)
   
-  d <- load_data(sample_path, factors_path)
-  items_data <- d$items_data
-  cols_tpsai <- d$item_ids
-  
-  factors_data <- items_data |>
-    select(all_of(cols_tpsai)) |>
+  factors_data <- data |>
     mutate(across(everything(), ~replace_na(.x, 4)))
   
   raw_cor <- cor(factors_data, method = 'spearman', use = "pairwise.complete.obs")
   xcors <- raw_cor |> 
     as_tibble(rownames = "item") |>
     pivot_longer(-item, names_to = "item2", values_to = "r") |>
-    mutate(item = gsub("_(\\d)$", "_0\\1", item),
-           item2 = gsub("_(\\d)$", "_0\\1", item2)) |>
     filter(item < item2) |>
     mutate(shrink = ifelse(r < 0, -1, 1) * abs(r) * shrinkage)|>
     arrange(item, item2)
